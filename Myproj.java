7107f0d30d35e229acdba5a64ec4b189b299d6ec
PRAJYOTDHUMAL <130650770+PRAJYOTDHUMAL@users.noreply.github.com>
2023-04-13 15:00:08 +0530

    Update Myproj.java

--- Myproj.java
+++ Myproj.java
@@ -2,6 +2,6 @@ public class Myproj
 {
 	public static void main(String args[])
 		{
-			System.out.println("WELCOME TO JENKINS....");
+			System.out.println("WELCOME TO JENKINS....FROM GITHUB");
 		}
 }
